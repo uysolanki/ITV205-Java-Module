75eb921e746af2695f928332d32d48cbbf463533
Umar Solanki <uysolanki@gmail.com>
2024-11-20 18:29:36 +0530

    day 7

--- src/day6/RotateArray.java
+++ src/day6/RotateArray.java
@@ -1,6 +1,7 @@
 package day6;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class RotateArray {
 
@@ -23,7 +24,52 @@ public class RotateArray {
 			arr[j]=temp;
 		}
 		System.out.println(Arrays.toString(arr));	//{6,7,8,9,2,3,4,5}
-
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number to Search");
+		int s=sc.nextInt();	
+		
+		int foundIndex;
+		if(s>=arr[0])
+		{
+			foundIndex=binarySearch(arr,0,(arr.length-1)/2, s);	
+		}
+		else
+		{
+			foundIndex=binarySearch(arr,((arr.length-1)/2)+1,arr.length-1, s);	
+		}
+		
+		if(foundIndex==-1)
+		{
+			System.out.println("Number Not Found");
+		}
+		else
+		{
+			System.out.println("Number Found at index "+foundIndex);
+		}
+	}
+	
+	private static int binarySearch(int arr[],int start, int end, int s) {
+		int mid;
+		while(start<=end)
+		{
+			mid=(start+end)/2;			
+			if(arr[mid]==s)
+			{
+				return mid;
+			}
+			else if(s>arr[mid])
+			{
+				start=mid+1;
+			}
+			else
+			{
+				end =mid-1;
+			}
+		}
+		
+			return -1;
+		
 	}
 
 }
---
+++ src/day7/BinarySearchDemo.java
@@ -0,0 +1,49 @@
+package day7;
+
+import java.util.Scanner;
+
+public class BinarySearchDemo {
+
+	public static void main(String[] args) {
+		int arr[]={2,3,4,5,6,7,8,9}; 
+			
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number to Search");
+		int s=sc.nextInt();			//s=7
+		
+		int foundIndex=binarySearch(arr,0,arr.length-1, s);
+		if(foundIndex==-1)
+		{
+			System.out.println("Number Not Found");
+		}
+		else
+		{
+			System.out.println("Number Found at index "+foundIndex);
+		}
+		
+	}
+
+	private static int binarySearch(int arr[],int start, int end, int s) {
+		int mid;
+		while(start<=end)
+		{
+			mid=(start+end)/2;			
+			if(arr[mid]==s)
+			{
+				return mid;
+			}
+			else if(s>arr[mid])
+			{
+				start=mid+1;
+			}
+			else
+			{
+				end =mid-1;
+			}
+		}
+		
+			return -1;
+		
+	}
+
+}
